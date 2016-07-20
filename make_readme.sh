60eae1389c455e6b581408b11dbf718e74f3d9ea
spookmango <spook@mango.com>
2016-07-20 05:06:55 -0400

    take 3 idgaf

--- make_readme.sh
+++ make_readme.sh
@@ -3,6 +3,6 @@ OUTPUT="../README.md"
 cd cloud_portal
 echo "<ul>" > $OUTPUT
 for f in *; do
-	echo "<li><a href=\"https://spookmango.github.io/slingshot-epub/" $f ">" $f "</a></li>" >> $OUTPUT
+	echo "<li><a href=\"https://spookmango.github.io/slingshot-epub/"$f"\">"$f"</a></li>" >> $OUTPUT
 done
 echo "</ul>" >> $OUTPUT
