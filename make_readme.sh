0056f30ea2408a1f42bd4bd671e65ef07cad42a6
spookmango <spook@mango.com>
2016-07-20 05:05:19 -0400

    links take 2

--- make_readme.sh
+++ make_readme.sh
@@ -2,7 +2,7 @@
 OUTPUT="../README.md"
 cd cloud_portal
 echo "<ul>" > $OUTPUT
-for f in ./*; do
-	echo "<li><a href=\"" $f ">" $f "</a></li>" >> $OUTPUT
+for f in *; do
+	echo "<li><a href=\"https://spookmango.github.io/slingshot-epub/" $f ">" $f "</a></li>" >> $OUTPUT
 done
 echo "</ul>" >> $OUTPUT
