49fc19fb15c78bb1700bd8c155ddf20e7609212b
spookmango <spook@mango.com>
2016-07-20 04:51:24 -0400

    cleared files

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -f /Volumes/Kindle/documents/parse.mobi
-cp parse.mobi /Volumes/Kindle/documents/
--- make_mobi.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir mobi
-for f in ./epub/*epub; do KindleGen_Mac_i386_v2_9/kindlegen $f; done
