b162651f56f6be60df72b169e1f7d0be84a4b10a
spookmango <etugur96@gmail.com>
2016-07-24 14:44:23 -0400

    Update make_mobi.sh

--- make_mobi.sh
+++ make_mobi.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# optional step before make_readme.sh if you want to support kindle 
+
+# download kindlegen from here https://www.amazon.com/gp/feature.html?docId=1000765211 and modify the line below as needed
+
 for f in ./cloud_portal/*epub; do KindleGen_Mac_i386_v2_9/kindlegen $f; done
